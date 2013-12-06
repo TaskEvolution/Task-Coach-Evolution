236fd4cc31f6f848aacfad6a4be151dca26333d0
Erik <erikduvet@gmail.com>
2013-12-06 20:10:09 +0100

    commit .exe, writer

--- taskcoach/taskcoachlib/persistence/pdf/writer.py
+++ taskcoach/taskcoachlib/persistence/pdf/writer.py
@@ -19,6 +19,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import re, generator, os
 from taskcoachlib.domain import date
 from xhtml2pdf import pisa
+from reportlab.pdfbase import _fontdata_enc_winansi
+from reportlab.pdfbase import _fontdata_enc_macroman
+from reportlab.pdfbase import _fontdata_enc_standard
+from reportlab.pdfbase import _fontdata_enc_symbol
+from reportlab.pdfbase import _fontdata_enc_zapfdingbats
+from reportlab.pdfbase import _fontdata_enc_pdfdoc
+from reportlab.pdfbase import _fontdata_enc_macexpert
+from reportlab.pdfbase import _fontdata_widths_courier
+from reportlab.pdfbase import _fontdata_widths_courierbold
+from reportlab.pdfbase import _fontdata_widths_courieroblique
+from reportlab.pdfbase import _fontdata_widths_courierboldoblique
+from reportlab.pdfbase import _fontdata_widths_helvetica
+from reportlab.pdfbase import _fontdata_widths_helveticabold
+from reportlab.pdfbase import _fontdata_widths_helveticaoblique
+from reportlab.pdfbase import _fontdata_widths_helveticaboldoblique
+from reportlab.pdfbase import _fontdata_widths_timesroman
+from reportlab.pdfbase import _fontdata_widths_timesbold
+from reportlab.pdfbase import _fontdata_widths_timesitalic
+from reportlab.pdfbase import _fontdata_widths_timesbolditalic
+from reportlab.pdfbase import _fontdata_widths_symbol
+from reportlab.pdfbase import _fontdata_widths_zapfdingbats
 
 
 class PDFWriter(object):
